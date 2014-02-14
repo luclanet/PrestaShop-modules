50e8ce47f0b4db0f9a73ba738802c669dedefa1a
Claudio Mulas <claudio.mulas@lucla.biz>
2014-02-14 17:20:02 +0100

    Update download.php

--- feedaty/download.php
+++ feedaty/download.php
@@ -37,6 +37,8 @@ $cryptToken = md5($idEmployee._COOKIE_KEY_.$timeGenerated);
 
 if (Tools::getValue('cryptToken') == $cryptToken && ((time() - $timeGenerated) < $max_time))
 {
+	$final_products = array();
+	
 	/* Retrive all orders up to 3 months */
 	$sql = 'SELECT *
 			FROM '._DB_PREFIX_.'orders
@@ -63,7 +65,6 @@ if (Tools::getValue('cryptToken') == $cryptToken && ((time() - $timeGenerated) <
 
 				if (is_array($products) && (count($products) > 0))
 				{
-					$final_products = array();
 					foreach ($products as $product)
 					{
 						/* For each product we collect image url, url, name, id */
@@ -89,14 +90,13 @@ if (Tools::getValue('cryptToken') == $cryptToken && ((time() - $timeGenerated) <
 
 	/* CSV Header */
 	$csv = '"Order ID","UserID","E-mail","Date","Product ID","Extra","Product Url","Product Image","Platform"'."\r\n";
-	if (isset($final_products) && is_array($final_products))
-		foreach ($final_products as $p)
-		{
-			/* Every row it's a product on order */
-			$csv .= '"'.$p['OrderId'].'","'.$p['CustomerId'].'","'.$p['CustomerEmail'].'","'.$p['OrderDate'].'","'.$p['Id'].'","'.
-				str_replace('"', '""', $p['Name']).'","'.str_replace('"', '""', $p['Url']).'","'.
-				str_replace('"', '""', $p['ImageUrl']).'","PrestaShop"'."\r\n";
-		}
+	foreach ($final_products as $p)
+	{
+		/* Every row it's a product on order */
+		$csv .= '"'.$p['OrderId'].'","'.$p['CustomerId'].'","'.$p['CustomerEmail'].'","'.$p['OrderDate'].'","'.$p['Id'].'","'.
+			str_replace('"', '""', $p['Name']).'","'.str_replace('"', '""', $p['Url']).'","'.
+			str_replace('"', '""', $p['ImageUrl']).'","PrestaShop"'."\r\n";
+	}
 
 	/* Send header to force download of a file called export_date.csv */
 	header('Cache-Control: public');
